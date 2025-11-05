5a0e32a6278b52cc5ef77516a17333d5c8a97747
c0smic3atr <marsdatrandomdemon@gmail.com>
2025-11-05 10:18:59 -0800

    did 21-26

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -1,28 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public static Action onShoot;
+
     public Transform BulletSpawnPoint => _bulletSpawnPoint;
 
     [SerializeField] private Transform _bulletSpawnPoint;
     [SerializeField] private Bullet _bulletPrefab;
-    
+    [SerializeField] private float _gunFireCD = 0.5f;
+
+    private static readonly int FIRE_HASH = Animator.StringToHash("Fire");
+    private Vector2 _mousePos;
+    private float _lastFireTime = 0f;
+
+    private Animator _animator;
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         Shoot();
+        RotateGun();
+    }
+
+    private void OnEnable()
+    {
+        onShoot += ShootProjectile;
+        onShoot += ResetLastFireTime;
+        onShoot += FireAnimation;
+    }
+
+    private void OnDisable()
+    {
+        onShoot -= ShootProjectile;
+        onShoot -= ResetLastFireTime;
+        onShoot -= FireAnimation;
     }
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            ShootProjectile();
+        if (Input.GetMouseButton(0) && Time.time >= _lastFireTime)
+        {
+            onShoot?.Invoke();
         }
     }
 
     private void ShootProjectile()
     {
+        
         Bullet newBullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, Quaternion.identity);
+        newBullet.Intit(_bulletSpawnPoint.position, _mousePos);
+    }
+
+    private void FireAnimation()
+    {
+        _animator.Play(FIRE_HASH, 0, 0f);
+    }
+
+    private void ResetLastFireTime()
+    {
+        _lastFireTime = Time.time + _gunFireCD;
+
+    }
+
+    private void RotateGun()
+    {
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = PlayerController.Instance.transform.InverseTransformPoint(_mousePos);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
